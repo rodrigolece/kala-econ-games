37aa7faf69d6ff76d70eca5511b03b938187202e
rodrigolece <rodrigolealc@ciencias.unam.mx>
2023-08-31 15:34:03 -0600

    Added top level exports

---
+++ kala/__init__.py
@@ -0,0 +1,5 @@
+# pylint: disable=unused-import
+from kala.models.agents import InvestorAgent
+from kala.models.game import DiscreteTwoByTwoGame
+from kala.models.graphs import SimpleGraph
+from kala.models.strategies import CooperationStrategy
