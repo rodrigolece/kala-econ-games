f129f29d84757e8a78804fb76895056bdb99a873
rodrigolece <rodrigolealc@ciencias.unam.mx>
2023-11-28 18:06:21 -0600

    Added support to add/remove nodes and edges

--- src/kala/models/graphs.py
+++ src/kala/models/graphs.py
@@ -1,7 +1,7 @@
 """Module defining the interface for the underlying graphs."""
 
 from abc import ABC, abstractmethod
-from typing import Any, Generic, Mapping, Sequence, TypeVar
+from typing import Any, Generic, Sequence, TypeVar
 
 import networkx as nx
 
@@ -17,12 +17,16 @@ class BaseGraph(ABC, Generic[AgentT]):
     get_node()
     get_neighbours()
     num_nodes()
+    add_node()
+    remove_node()
+    add_edge()
+    remove_edge()
 
     """
 
     _graph: Any
-    _nodes: Sequence[AgentT]
-    _addition_order: Mapping[str, int]
+    _nodes: list[AgentT]
+    _addition_order: dict[str, int]
 
     @abstractmethod
     def get_node(self, nid: int | str) -> AgentT:
@@ -40,15 +44,31 @@ class BaseGraph(ABC, Generic[AgentT]):
         """Return the number of nodes in the graph."""
         return len(self._nodes)
 
+    @abstractmethod
+    def add_node(self, node: AgentT) -> bool:
+        """Add the node to the graph."""
+
+    @abstractmethod
+    def remove_node(self, node: AgentT | int | str) -> bool:
+        """Remove the node from the graph."""
+
+    @abstractmethod
+    def add_edge(self, u: AgentT | int | str, v: AgentT | int | str) -> bool:
+        """Add an edge between nodes u and v (the nodes should already be in the graph)."""
+
+    @abstractmethod
+    def remove_edge(self, u: AgentT | int | str, v: AgentT | int | str) -> bool:
+        """Remove the edge between nodes u and v."""
+
 
 GraphT = TypeVar("GraphT", bound=BaseGraph)
 """Used to refer to BaseGraph as well as its subclasses."""
 
 
-class SimpleGraph(BaseGraph):
+class SimpleGraph(BaseGraph, Generic[AgentT]):
     """Unweighted, undirected graph built on top of NetworkX."""
 
-    _nodes: Sequence[AgentT | None]
+    _nodes: list[AgentT | None]
 
     def __init__(self, graph: nx.Graph, nodes: Sequence[AgentT]):
         self._graph = graph
@@ -58,7 +78,7 @@ class SimpleGraph(BaseGraph):
         # NB: nodes is relied upon and cannot be None in this implementation
         for i, n in enumerate(nodes):
             self._nodes.append(n)
-            nid = getattr(n, "uuid", i)
+            nid = getattr(n, "uuid", str(i))
             self._addition_order[nid] = i
 
     def _get_pos_from_node(self, node: AgentT | int | str) -> int:
@@ -72,11 +92,67 @@ class SimpleGraph(BaseGraph):
             raise TypeError(f"Invalid type for node: {type(node)}")
         return node
 
-    def get_node(self, nid: int | str) -> AgentT:
-        pos: int = self._get_pos_from_node(nid)  # type: ignore
+    def num_nodes(self) -> int:
+        # NB: this is specific to NetworkX because we allow None so as to not invalidate
+        # the self._addition_order
+        return len(list(filter(None, self._nodes)))
+
+    def get_node(self, nid: int | str) -> AgentT | None:
+        # FIXME: return could be node if the node is removed
+        pos = self._get_pos_from_node(nid)  # type: ignore
         return self._nodes[pos]
 
     def get_neighbours(self, node: AgentT | int | str) -> Sequence[AgentT]:
         pos = self._get_pos_from_node(node)
         _neighs = self._graph.neighbors(pos)
-        return [self.get_node(i) for i in _neighs]
+        return list(filter(None, (self.get_node(i) for i in _neighs)))
+
+    def add_node(self, node: AgentT) -> bool:
+        pos = len(self._nodes)  # no +1 bcs python is 0-indexed
+
+        if getattr(node, "uuid", None) in self._addition_order:
+            # node is already in the graph (networkx does not raise an error)
+            return False
+
+        self._graph.add_node(pos)
+        self._nodes.append(node)
+        self._addition_order[getattr(node, "uuid", str(pos))] = pos
+
+        return True
+
+    def remove_node(self, node: AgentT | int | str) -> bool:
+        pos = self._get_pos_from_node(node)
+        try:
+            self._graph.remove_node(pos)
+            # NB: networkx does not invalidate node labels so we cannot change self._addition_order
+            self._nodes[pos] = None
+
+            return True
+        except nx.NetworkXError:
+            return False
+
+    def add_edge(self, u: AgentT | int | str, v: AgentT | int | str) -> bool:
+        try:
+            pos_u = self._get_pos_from_node(u)
+            pos_v = self._get_pos_from_node(v)
+
+        except KeyError:  # at least one of the nodes is not in the graph
+            return False
+
+        if self._graph.has_edge(pos_u, pos_v):
+            return False
+
+        if pos_u == pos_v:  # self-loops are not allowed
+            return False
+
+        self._graph.add_edge(pos_u, pos_v)
+        return True
+
+    def remove_edge(self, u: AgentT | int | str, v: AgentT | int | str) -> bool:
+        pos_u = self._get_pos_from_node(u)
+        pos_v = self._get_pos_from_node(v)
+        try:
+            self._graph.remove_edge(pos_u, pos_v)
+            return True
+        except nx.NetworkXError:
+            return False
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,11 +1,12 @@
 """Test the graphs module."""
 
+import networkx as nx
 
 from kala.models.agents import InvestorAgent
 
 
 def test_get_functions_by_agents(init_simple_graph):
-    """Test the methods get_node and get_neighbours and the return type of the node"""
+    """Test the methods get_node and get_neighbours and the return type of the node."""
     G = init_simple_graph  # pylint: disable=invalid-name
 
     # get_node using the identifier
@@ -20,6 +21,7 @@ def test_get_functions_by_agents(init_simple_graph):
 
 
 def test_get_functions_by_string(init_simple_graph):
+    """Test the methods get_node and get_neighbours using a string identifier."""
     G = init_simple_graph  # pylint: disable=invalid-name
 
     neighs = G.get_neighbours("a")
@@ -30,6 +32,7 @@ def test_get_functions_by_string(init_simple_graph):
 
 
 def test_get_functions_by_pos(init_simple_graph):
+    """Test the methods get_node and get_neighbours using an integer (position)."""
     G = init_simple_graph  # pylint: disable=invalid-name
 
     n0 = G.get_node(0)  # type: ignore
@@ -41,3 +44,54 @@ def test_get_functions_by_pos(init_simple_graph):
     assert all(isinstance(n, InvestorAgent) for n in neighs)
 
     assert len(G.get_neighbors(2)) == 3
+
+
+def test_add_node(init_simple_graph):
+    """Test the method add_node."""
+    G = init_simple_graph  # pylint: disable=invalid-name
+
+    # NB: we are not adding the nodes in order
+    agent = InvestorAgent(is_saver=False, uuid="g")
+    assert G.add_node(agent)
+    assert G.num_nodes() == 7
+
+    assert G.get_node(agent).uuid == "g"
+    assert G.get_node("g") == agent
+    assert G.get_node(6) == agent
+
+    assert G.add_node(agent) is False  # the second time it should fail
+
+
+def test_remove_node(init_simple_graph):
+    """Test the method remove_node."""
+    G = init_simple_graph  # pylint: disable=invalid-name
+
+    # NB: we are not removing the nodes in order
+    assert G.remove_node("e")
+    assert G.num_nodes() == 5
+
+    assert G.remove_node("f")
+    assert G.num_nodes() == 4
+
+    assert G.remove_node("f") is False
+
+
+def test_add_edge(init_simple_graph):
+    """Test the method add_edge."""
+    G = init_simple_graph  # pylint: disable=invalid-name
+
+    assert G.add_edge("a", "b") is False  # the edge already exists
+    assert G.add_edge("a", "a") is False  # self-loops are not allowed
+    assert G.add_edge("a", "g") is False  # the node does not exist
+
+    assert G.add_edge("a", "d")
+    assert nx.number_of_edges(G._graph) == 8  # pylint: disable=protected-access
+
+
+def test_remove_edge(init_simple_graph):
+    """Test the method remove_edge."""
+    G = init_simple_graph  # pylint: disable=invalid-name
+
+    assert G.remove_edge("c", "d")
+    assert nx.number_of_edges(G._graph) == 6  # pylint: disable=protected-access
+    assert nx.is_connected(G._graph) is False  # pylint: disable=protected-access
