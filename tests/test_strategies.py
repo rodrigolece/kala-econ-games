2bbe4faaf9eff60e665117d3828aca9c2f4f9476
rodrigolece <rodrigolealc@ciencias.unam.mx>
2024-10-17 09:36:20 -0600

    Fixed log-normal parameters which shifted the mean

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -16,11 +16,11 @@ def test_init_cooperation_strategy(agent_pair):
     """Test the initialization of a cooperation strategy."""
     saver, non_saver = agent_pair
 
-    strategy = CooperationStrategy(stochastic=False, rng=0, dist_sigma_func=lambda x: 1e-6)
-    efficient, inefficient = 1.15, 0.9
-    assert not strategy.stochastic
+    strategy = CooperationStrategy(rng=0, dist_sigma_func=lambda x: 1e-6)
+    pay_ss, pay_sn = 1.15, 0.9
+    assert strategy.stochastic  # stochastic=True by default
 
-    assert strategy.calculate_payoff(saver, saver) == pytest.approx([efficient, efficient])
-    assert strategy.calculate_payoff(saver, non_saver) == pytest.approx([inefficient, 1.0])
-    assert strategy.calculate_payoff(non_saver, saver) == pytest.approx([1.0, inefficient])
+    assert strategy.calculate_payoff(saver, saver) == pytest.approx([pay_ss, pay_ss], rel=1e-3)
+    assert strategy.calculate_payoff(saver, non_saver) == pytest.approx([pay_sn, 1.0], rel=1e-3)
+    assert strategy.calculate_payoff(non_saver, saver) == pytest.approx([1.0, pay_sn], rel=1e-3)
     assert strategy.calculate_payoff(non_saver, non_saver) == pytest.approx([1.0, 1.0])
